db7d9366c82c32cae4448e86a75cd64dd335e298
jihenebahi <jihenebahi@gmail.com>
2026-03-02 12:46:33 +0100

    ajout d'autre fonctionaliter

--- src/services/infoCentre/categorieService.js
+++ src/services/infoCentre/categorieService.js
@@ -1,5 +1,7 @@
 // src/services/infoCentre/categorieService.js
 import api from '../api';
 
-export const getCategories = () => api.get('categories/');
-export const ajouterCategorie = (data) => api.post('categories/ajouter/', data);
+export const getCategories      = ()           => api.get(`categories/`);
+export const ajouterCategorie   = (data)       => api.post(`categories/ajouter/`, data);
+export const modifierCategorie  = (id, data)   => api.put(`categories/${id}/modifier/`, data);
+export const supprimerCategorie = (id)         => api.delete(`categories/${id}/supprimer/`);
