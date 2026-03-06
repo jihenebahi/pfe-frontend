faef96801e0a263ec59e4445c879db9429ea52ed
yesmine <yesminemaaoui@gmail.com>
2026-03-06 13:46:52 +0100

    gestion de formation

--- src/services/infoCentre/formationService.js
+++ src/services/infoCentre/formationService.js
@@ -10,3 +10,5 @@ export const ajouterFormation = (data) => api.post('formations/ajouter/', data);
 export const modifierFormation = (id, data) => api.put(`formations/modifier/${id}/`, data);
 
 export const supprimerFormation = (id) => api.delete(`formations/supprimer/${id}/`);
+
+export const getFormateursDisponibles = () =>api.get("formations/formateurs-disponibles/");
